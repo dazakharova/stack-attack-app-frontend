025a55d55603da6d7eac0cfc64fc8cdbfd676c76
Daria Zakharova <t3zada00@students.oamk.fi>
2024-04-04 09:26:57 +0300

    Fix modal ID assignment, deduplicate JS code, and implement form window switching

--- js/login.js
+++ js/login.js
@@ -2,31 +2,29 @@ document.addEventListener("DOMContentLoaded", function () {
   //search for the login form
   const loginForm = document.getElementById("loginForm");
   const modal = document.getElementById("loginModal");
-  const btn = document.getElementById("loginLink");
-  const span = document.getElementsByClassName("close")[0];
- 
   const loginLink = document.getElementById("loginLink");
+  const closeButton = document.getElementsByClassName("close")[1];
   
   loginLink.addEventListener("click", function (event) {
     event.preventDefault();
-    
-    // When the user clicks on the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-  document.body.style.overflow = "hidden"; // Prevent scrolling
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-  document.body.style.overflow = "auto"; // Enable scrolling
-}
- 
-});
-// Find a button to close the login form and add the event to close it
-document.querySelector(".closeLoginForm").addEventListener("click", function () {
-  modal.style.display = "none";
-  document.body.style.overflow = "auto"; // Enable scrolling
-});
+    // When the user clicks on the button, open the modal
+    modal.style.display = "block";
+    document.body.style.overflow = "hidden"; // Prevent scrolling
+  });
+
+  // When the user clicks on <span> (x), close the modal
+  closeButton.onclick = function() {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto"; // Enable scrolling
+  }
+
+  // When the user clicks to switch to SignUp
+  document.getElementById("switchToSignup").addEventListener("click", function() {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto";
+    document.getElementById("signupModal").style.display = "block";
+    document.body.style.overflow = "hidden";
+  })
 
   loginForm.addEventListener("submit", function (event) {
     //
@@ -66,18 +64,18 @@ document.querySelector(".closeLoginForm").addEventListener("click", function () 
       body: JSON.stringify(data),
     };
 
-});
-
     // send to server
     fetch(BACKEND_ROOT_URL + "/auth/login", options)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        console.log("Data sent successfully!");
-      })
-      .catch((error) => {
-        console.error("There was an error!", error);
-      });
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          console.log("Data sent successfully!");
+        })
+        .catch((error) => {
+          console.error("There was an error!", error);
+        });
   });
+
+});
 
--- js/register.js
+++ js/register.js
@@ -1,42 +1,27 @@
 const BACKEND_ROOT_URL = "http://localhost:3001";
 const registrationForm = document.getElementById("registrationForm");
 const modal = document.getElementById("signupModal");
-const btn = document.getElementById("signupLink");
-const span = document.getElementsByClassName("close")[0];
-
-// registration (put into header)
 const signupLink = document.getElementById("signupLink");
+const closeButton = document.getElementsByClassName("close")[0];
 
 signupLink.addEventListener("click", function (event) {
   event.preventDefault();
-
-  // When the user clicks on the button, open the modal 
-btn.onclick = function() {
+  // When the user clicks on the button, open the modal
   modal.style.display = "block";
   document.body.style.overflow = "hidden"; // Prevent scrolling
-}
+});
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+closeButton.onclick = function() {
   modal.style.display = "none";
   document.body.style.overflow = "auto"; // Enable scrolling
 }
 
-});
-
-// Find a button to close the form and add the event to close it
-document
-  .getElementById("closeRegistrationForm")
-  .addEventListener("click", function () {
-    document.getElementById("registrationForm").style.display = "none";
-    document.querySelector(".overlay-registration").style.display = "none";
-  });
-
-  document.getElementById("loginLink").addEventListener("click", function () {
-    document.getElementById("registrationForm").style.display = "none";
-   
-    document.querySelector(".overlay-registration").style.display = "none"
- 
+  document.getElementById("switchToLogin").addEventListener("click", function () {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto";
+    document.getElementById("loginModal").style.display = "block";
+    document.body.style.overflow = "hidden";
   });
 
 
@@ -44,42 +29,35 @@ registrationForm.addEventListener("submit", function (event) {
   //
   event.preventDefault(); // prevent default
 
-  const usernameInput = document.getElementById("registerUsername");
+  const nameInput = document.getElementById("registerName");
+  const emailInput = document.getElementById("registerEmail")
   const passwordInput = document.getElementById("registerPassword");
 
-  const username = usernameInput.value;
+  // const name = nameInput.value;
+  const email = emailInput.value;
   const password = passwordInput.value;
-
-  //regex for username
-  const usernameRegex = /^[a-zA-Z0-9_]{4,}$/;
 
   //regex for password
   const passwordRegex =
     /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$%^&!])[A-Za-z\d@#$%^&!]{8,}$/;
 
-  // regex to check username:
-  //"Username must be at least 4 characters long and can only include letters (a-z, A-Z), numbers (0-9), and underscores (_).
-  if (!usernameRegex.test(username)) {
-    alert("Please choose a new username");
-    return; // interrupt the code
-  }
   //regex to check password
   //"Password must be at least 8 characters long and include at least one letter, one number, and one special character (e.g., @, #, $, %)."
-  if (!passwordRegex.test(password)) {
-    alert("Please create a more secure password");
-    return; // interrupt the code
-  }
+  // if (!passwordRegex.test(password)) {
+  //   alert("Please create a more secure password");
+  //   return; // interrupt the code
+  // }
   //in case of succesfull check
-  sendDataToBackend(username, password);
+  sendDataToBackend(email, password);
 });
 
-function sendDataToBackend(username, password) {
+function sendDataToBackend(email, password) {
   const requestOptions = {
     method: "POST",
     headers: {
       "Content-type": "application/json",
     },
-    body: JSON.stringify({ username, password }),
+    body: JSON.stringify({ email, password }),
   };
 
   fetch(BACKEND_ROOT_URL + "/auth/register", requestOptions)
