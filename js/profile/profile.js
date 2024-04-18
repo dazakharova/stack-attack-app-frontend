e07fe5fc9cc9bd7dad3221e4c80b403c9afef051
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-04-18 23:20:58 +0300

    Change left-container's menu to fix the style. Part1

--- js/profile/profile.js
+++ js/profile/profile.js
@@ -15,9 +15,9 @@ const roomsHierarchy = document.getElementById("roomsHierarchy")
 
 const processRooms = (data) => {
     // Exclude first 3 elements of left container (heading and 2 buttons) from removing before rendering all rooms
-    while (roomsHierarchy.children.length > 3) {
+    /*while (roomsHierarchy.children.length > 3) {
         roomsHierarchy.removeChild(roomsHierarchy.lastChild);
-    }
+    }*/
     // Extract only parent containers from the given data (which have 'null' as a parent_id):
     const roomsArray = data.get(null)
     console.log("Rooms array", roomsArray)
