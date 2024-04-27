bfa1bdaafaf9f92094d8edd3d8c242e3ac1a050d
Daria Zakharova <t3zada00@students.oamk.fi>
2024-04-27 06:24:17 +0300

    Remove redundant code

--- js/scroll.js
+++ js/scroll.js
@@ -1,62 +1,7 @@
-/*document.addEventListener("DOMContentLoaded", function() {
-  // Find the link to Our Team
-  const ourTeamLink = document.querySelector("nav .navbar_nav li:nth-child(1) a");
-
-  // Find the link to Feedback
-  const feedbackLink = document.querySelector("nav .navbar_nav li:nth-child(2) a");
-
-  // Function to scroll to the specified section
-  function scrollToSection(section) {
-    section.scrollIntoView({ behavior: 'smooth' });
-  }
-
-  // Add event listener for clicking on the Our Team link
-  ourTeamLink.addEventListener("click", function(event) {
-    event.preventDefault();
-    const ourTeamSection = document.querySelector(".ourTeam");
-    scrollToSection(ourTeamSection);
-  });
-
-  // Add event listener for clicking on the Feedback link
-  feedbackLink.addEventListener("click", function(event) {
-    event.preventDefault();
-    const feedbackSection = document.querySelector(".feedback");
-    scrollToSection(feedbackSection);
-  });
-});
-*/
-
-
-
-
 document.addEventListener("DOMContentLoaded", function() {
-  // Find the link to Our Team
-  const ourTeamLink = document.querySelector("nav .navbar_nav li:nth-child(1) a");
-
-  // Find the link to Feedback
-  const feedbackLink = document.querySelector("nav .navbar_nav li:nth-child(2) a");
 
   // Find the logged header
   const loggedHeader = document.getElementById("loggedHeader");
-
-  // Function to scroll to the specified section
-  function scrollToSection(section) {
-    section.scrollIntoView({ behavior: 'smooth' });
-  }
-
-  // Add event listener for clicking on the Our Team link
-  ourTeamLink.addEventListener("click", function(event) {
-    event.preventDefault();
-    const ourTeamSection = document.querySelector(".ourTeam");
-    scrollToSection(ourTeamSection);
-  });
-
-  // Add event listener for clicking on the Feedback link
-  feedbackLink.addEventListener("click", function(event) {
-    event.preventDefault();
-    const feedbackSection = document.querySelector(".feedback");
-    scrollToSection(feedbackSection);
-  });
 
   let lastScrollTop = 0;
   let scrollingTimer;
