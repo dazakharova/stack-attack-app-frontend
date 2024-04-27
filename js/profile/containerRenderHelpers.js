6fd5db2e6ce971fb97f2d69834bbb8e4414c61fc
Daria Zakharova <t3zada00@students.oamk.fi>
2024-04-27 12:07:15 +0300

    Refactor functions for item rendering in right section of the page

--- js/profile/containerRenderHelpers.js
+++ js/profile/containerRenderHelpers.js
@@ -1,9 +1,5 @@
-import {Container} from "../class/Container.js";
-import {Item} from "../class/Item.js";
-import leftContainer from "./collapseFunctionality.js";
-import {assets, getBase64FromImageInput} from "./profile.js";
-import { setupConfirmationModal } from './utils/uiDynamicUpdate.js'
 import { createContainerDiv, createContainerFooter, createTitleSpan, handleContainerClick, setupNameEdit, setupNameSave, setupDelete } from './utils/rightContainerRenderer.js';
+import { getItemData, createItemDiv, createItemImage, createItemSpan, handleItemDivClick } from "./utils/rightItemRenderer.js";
 
 function renderContainer(parentNode, container, data) {
     const containerId = container.getId();
@@ -28,163 +24,18 @@ function renderContainer(parentNode, container, data) {
 }
 
 const renderItem = (parentNode, item, data) => {
-    const itemId = item.getId()
-    const itemName = item.getName()
-    const itemParentId = item.getContainerId()
-    const itemImageStr = item.getImage()
-    // Create div for item
-    const itemDiv = document.createElement('div')
-    itemDiv.className = 'item'
+    const { itemId, itemName, itemDescription, itemParentId, itemImageStr } = getItemData(item)
+    const itemDiv = createItemDiv()
 
-    // Add event listener to item div, once it's clicked - popup window with detailed info about the item shows up
     itemDiv.onclick = () => {
-        // Get the modal window
-        const itemModal = document.getElementById("item-modal")
-
-        // Get the elements of the modal window after opening it
-        const modalImage = document.getElementById("modal-image");
-        const modalTitle = document.getElementById("modal-title");
-        const modalDescription = document.getElementById("modal-description");
-
-        // Set the data in the modal window
-        modalTitle.textContent = item.getName();
-        if (itemImageStr) {
-            modalImage.src = `data:image/jpeg;base64,${itemImageStr}`
-        }
-        modalDescription.textContent = item.getDescription();
-
-
-        // Display the modal window
-        itemModal.style.display = "block";
-
-        // Edit item name button
-        const editItemNameBtn = document.getElementById('edit-item-name')
-
-        // Get newItemNameDiv with input and submit button
-        const newItemNameDiv = document.querySelector('.new-item-name-div')
-        const newItemNameInput = document.querySelector('.new-item-name-div .item-title-input')
-        const okButton = document.querySelector('.new-item-name-div .ok-button')
-
-        // When edit button is clicked, inout for new item name show up
-        editItemNameBtn.onclick = () => {
-            replaceItemNameWithEditableInput(newItemNameDiv, newItemNameInput, modalTitle)
-        }
-
-        // When submit button is clicked, name gets updated and left menu and right container on the page update their contents
-        okButton.onclick = () => {
-            return updateItemNameAndRefreshUI(modalTitle, newItemNameInput, newItemNameDiv, itemId)
-        }
-
-        const editDescriptionBtn = document.getElementById('edit-item-description')
-
-        // Get new item description div with input and submit button
-        const newDescriptionDiv = document.getElementById('new-description-div')
-        const descriptionInput = document.querySelector('.item-description-input')
-        const okDescriptionBtn = document.querySelector('.item-description-ok')
-
-        // When edit button is clicked, inout for new item name show up
-        editDescriptionBtn.onclick = () => {
-            replaceItemDescriptionWithEditableInput(modalDescription, newDescriptionDiv, descriptionInput)
-        }
-
-        // When submit button is clicked, description gets updated and left menu and right container on the page update their contents
-        okDescriptionBtn.onclick = () => {
-            return updateItemDescriptionAndRefreshUI(modalDescription, newDescriptionDiv, descriptionInput, itemId)
-        }
-
-        const editImageBtn = document.getElementById('edit-item-photo')
-
-        editImageBtn.onclick = () => {
-            displayImageUploadInput(itemId, modalImage)
-        }
-
-        // Delete item button
-        const deleteItemBtn = document.getElementById('delete-item')
-
-        // When delete item button is clicked, the user gets confirmation window, if confirmed
-        // item window gets closed and left menu and right container on the page update their contents
-        deleteItemBtn.onclick = (event) => {
-            return handleItemDeletion(event, itemName, itemId, itemParentId, itemModal, parentNode, data)
-        }
-
-        // Close the modal window when the close button is clicked
-        let closeButton = document.querySelector("#close-item");
-        if (closeButton) {
-            closeButton.onclick = function () {
-                handleClosingItemModalWindow(newItemNameDiv, modalTitle, itemModal, parentNode, data, itemParentId)
-            }
-        }
+        handleItemDivClick(itemId, itemParentId, itemName, itemDescription, itemImageStr, parentNode, data)
     }
 
-
-
-    // Create img for item
-    const itemImg = document.createElement('img')
-    itemImg.className = "item-image"
-
-    if (itemImageStr) {
-        itemImg.src = `data:image/jpeg;base64,${itemImageStr}`
-    }
-
-
-
-    // Create span for item with its title
-    const itemSpan = document.createElement('span')
-    itemSpan.className = 'title'
-    itemSpan.innerText = item.getName()
-
-    // Set attributes holding unique information about an item in order to be able to access them later
-    itemSpan.setAttribute('data-id', item.getId())
-    itemSpan.setAttribute('data-containerId', item.getContainerId())
-
-    // Append item image and span to the item div
-    itemDiv.appendChild(itemImg)
-    itemDiv.appendChild(itemSpan)
+    createItemImage(itemImageStr, itemDiv)
+    createItemSpan(itemId, itemName, itemParentId, itemDiv)
 
     // Append item div to parent node given as an argument
     parentNode.appendChild(itemDiv)
-}
-
-const rightContainer = {
-    renderContainer,
-    renderItem
-}
-
-
-
-export default rightContainer
-
-const updateContentsInLeftMenu = (entityParentId, data) => {
-    // Get name of parent name (which is used in its children tags id)
-    const parentName = document.getElementById('location-info').lastElementChild.innerText
-
-    // Get parent node for items and containers in left menu
-    const parentNode= document.querySelector(`#${parentName.replace(/\s/g, '')}${entityParentId}-collapse > .containers-list`)
-
-    // Clean areas in parent containers node to draw it with updated data
-    parentNode.innerHTML = ''
-
-    // Rerender each entity inside the parent container of the current item
-    if (data.get(parseInt(entityParentId))) {
-        data.get(parseInt(entityParentId)).forEach(entity => {
-            if (entity instanceof Container) {
-                leftContainer.renderContainer(parentNode, entity, data)
-            }
-        })
-    }
-}
-
-const updateContentsInRightContainer = (parentNode, contents, data) => {
-    parentNode.innerHTML = ''
-    if (contents) {
-        contents.forEach(c => {
-            if (c instanceof Container) {
-                renderContainer(parentNode, c, data)
-            } else if (c instanceof Item) {
-                renderItem(parentNode, c, data)
-            }
-        })
-    }
 }
 
 const buildNewContainerNameInput = (containerDivFooter) => {
@@ -204,124 +55,6 @@ const buildNewContainerNameInput = (containerDivFooter) => {
 
     containerDivFooter.appendChild(newContainerNameDiv)
     return { okNameBtn: okButton }
-}
-
-// updateContainerNameAndRefreshUI(containerSpan, containerId, containerParentId, data)
-
-
-const replaceItemNameWithEditableInput = (newItemNameDiv, newItemNameInput, modalTitle) => {
-    // Replace item title with new div for editing
-    newItemNameDiv.style.display = 'block'
-
-    newItemNameInput.value = modalTitle.innerText
-
-    modalTitle.style.display = 'none'
-
-    // Focus on the input and select its content
-    newItemNameInput.focus();
-    newItemNameInput.select();
-}
-
-const updateItemNameAndRefreshUI = async (modalTitle, newItemNameInput, newItemNameDiv, itemId) => {
-    try {
-        modalTitle.textContent = newItemNameInput.value;
-        newItemNameDiv.style.display = 'none'
-        modalTitle.style.display = 'block'
-
-
-        const response = await assets.editItemName(itemId, modalTitle.textContent)
-
-    } catch (error) {
-        console.error(error)
-    }
-}
-
-const replaceItemDescriptionWithEditableInput = (modalDescription, newDescriptionDiv, descriptionInput) => {
-    modalDescription.style.display = 'none'
-    newDescriptionDiv.classList.add('new-item-description-div')
-    newDescriptionDiv.classList.remove('hidden')
-
-    descriptionInput.value = modalDescription.textContent
-
-    // Focus on the input and select its content
-    descriptionInput.focus();
-    descriptionInput.select();
-}
-
-const updateItemDescriptionAndRefreshUI = async (modalDescription, newDescriptionDiv, descriptionInput, itemId) => {
-    try {
-        modalDescription.textContent = descriptionInput.value;
-        newDescriptionDiv.classList.remove('new-item-description-div')
-        newDescriptionDiv.classList.add('hidden')
-        modalDescription.style.display = 'block'
-
-        const response = await assets.editItemDescription(itemId, modalDescription.textContent)
-
-    } catch (error) {
-        console.error(error)
-    }
-}
-
-const handleItemDeletion = async (event, itemName, itemId, itemParentId, itemModal, parentNode, data) => {
-    // Setup the confirmation modal and pass the confirm logic as a callback function
-    const confirmBtn = setupConfirmationModal(itemName, async () => {
-        try {
-            // Close the modal
-            document.getElementById('confirmation-modal').style.display = 'none'
-
-            // API call to remove the item
-            const response = await assets.removeItem(itemId)
-
-            // Prevent the event from bubbling
-            event.stopPropagation()
-
-            // Remove item modal window
-            itemModal.style.display = "none";
-
-            // Rerender left menu container with updated contents
-            updateContentsInLeftMenu(itemParentId, data)
-
-            const itemParentContents = data.get(parseInt(itemParentId))
-            // Re-render all the contents of the current container
-            updateContentsInRightContainer(parentNode, itemParentContents, assets.getAssets())
-        } catch (error) {
-            console.error(error)
-        }
-    })
-}
-
-const handleClosingItemModalWindow = (newItemNameDiv, modalTitle, itemModal, parentNode, data, itemParentId) => {
-        newItemNameDiv.style.display = 'none'
-        modalTitle.style.display = 'block'
-
-        itemModal.style.display = "none";
-
-        const itemParentContents = data.get(parseInt(itemParentId))
-
-        // Re-render all the contents of the current container
-        updateContentsInLeftMenu(itemParentId, data)
-        updateContentsInRightContainer(parentNode, itemParentContents, data)
-}
-
-const displayImageUploadInput = (id, modalImage) => {
-    const newItemImageInput = document.getElementById('new-item-image-input')
-    newItemImageInput.style.display = 'block'
-
-    newItemImageInput.onchange = () => handleImageInputChange(newItemImageInput, id, modalImage)
-}
-
-const handleImageInputChange = async (newItemImageInput, id, modalImage) => {
-        if (newItemImageInput.files.length > 0) {
-            const base64Image = await getBase64FromImageInput(newItemImageInput)
-            await assets.editItemImage(id, base64Image)
-            modalImage.src = `data:image/jpeg;base64,${base64Image}`
-
-            // Reset the file input after the image has been processed
-            newItemImageInput.value = '';
-
-            // Hide the file input
-            newItemImageInput.style.display = 'none';
-        }
 }
 
 function buildContainerDropdown(containerDiv) {
@@ -370,3 +103,10 @@ function buildContainerDropdown(containerDiv) {
         deleteContainerBtn: deletePlaceLink,
     };
 }
+
+const rightContainer = {
+    renderContainer,
+    renderItem
+}
+
+export default rightContainer
--- js/profile/utils/uiDynamicUpdate.js
+++ js/profile/utils/uiDynamicUpdate.js
@@ -2,7 +2,6 @@ import {Container} from "../../class/Container.js";
 import leftContainer from "../collapseFunctionality.js";
 import {Item} from "../../class/Item.js";
 import {addContainerToPath} from "../locationPathControls.js";
-import {assets} from "../profile.js";
 import rightContainer from "../containerRenderHelpers.js";
 
 const updateContentsInLeftMenu = (entityParentId, data) => {
@@ -60,179 +59,6 @@ const renderContainerContents = (event, parentNode, containerContents, container
     }
 }
 
-const buildNewContainerNameInput = (containerDivFooter, containerSpan) => {
-    const newContainerNameDiv = document.createElement('div')
-    newContainerNameDiv.className = 'new-container-name-div'
-
-    const newContainerNameInput = document.createElement('input')
-    newContainerNameInput.type = 'text'
-    newContainerNameInput.classList.add('title-input')
-
-    const okButton = document.createElement('button')
-    okButton.textContent = 'OK'
-    okButton.classList.add('ok-button')
-
-    newContainerNameDiv.appendChild(newContainerNameInput)
-    newContainerNameDiv.appendChild(okButton)
-
-    containerDivFooter.appendChild(newContainerNameDiv)
-}
-
-const replaceTitleWithEditableInput = (containerDivFooter, containerSpan, editIcon) => {
-    containerSpan.style.display = 'none'
-    editIcon.style.display = 'none'
-
-    // Show input new container name
-    const newContainerNameDiv = document.querySelector('.new-container-name-div')
-    newContainerNameDiv.style.display = 'block'
-
-    // Put the value of the container name into the input field
-    const newContainerNameInput = document.querySelector('.title-input')
-    newContainerNameInput.value = containerSpan.textContent
-
-    // Focus on the input and select its content
-    newContainerNameInput.focus()
-    newContainerNameInput.select()
-}
-
-const updateContainerNameAndRefreshUI = async (editIcon, containerSpan, containerId, containerParentId, data) => {
-    // Get parent name from the location path
-    const parentName = document.getElementById('location-info').lastElementChild.innerText
-
-    const newContainerNameDiv = document.querySelector('.new-container-name-div')
-
-    const newContainerNameInput = document.querySelector('.title-input')
-
-    try {
-        // Update container name in UI
-        containerSpan.textContent = newContainerNameInput.value
-        newContainerNameDiv.style.display = 'none'
-
-        containerSpan.style.display = 'block'
-        editIcon.style.display = 'block'
-
-
-        // Apply container name changes in local Map and send them to the server
-        const response = await assets.editContainerName(containerId, newContainerNameInput.value)
-
-        updateContentsInLeftMenu(containerParentId, data)
-    } catch (error) {
-        console.error(error)
-    }
-}
-
-const handleContainerDeletion = async (event, containerName, containerId, containerDiv, parentNode, containerParentId, data) => {
-    const isConfirmed = confirm(`Are you sure you want to delete "${containerName}"?`);
-
-    if (isConfirmed) {
-        try {
-            const response = await assets.removeContainer(containerId)
-
-            // Prevent the event from bubbling up to the room button click listener
-            event.stopPropagation();
-
-            parentNode.removeChild(containerDiv)
-
-            updateContentsInLeftMenu(containerParentId, data)
-        } catch (error) {
-            console.error(error)
-        }
-    }
-}
-
-const replaceItemNameWithEditableInput = (newItemNameDiv, newItemNameInput, modalTitle) => {
-    // Replace item title with new div for editing
-    newItemNameDiv.style.display = 'block'
-
-    newItemNameInput.value = modalTitle.innerText
-
-    modalTitle.style.display = 'none'
-
-    // Focus on the input and select its content
-    newItemNameInput.focus();
-    newItemNameInput.select();
-}
-
-const updateItemNameAndRefreshUI = async (modalTitle, newItemNameInput, newItemNameDiv, itemId) => {
-    try {
-        modalTitle.textContent = newItemNameInput.value;
-        newItemNameDiv.style.display = 'none'
-        modalTitle.style.display = 'block'
-
-
-        const response = await assets.editItemName(itemId, modalTitle.textContent)
-
-    } catch (error) {
-        console.error(error)
-    }
-}
-
-const replaceItemDescriptionWithEditableInput = (modalDescription, newDescriptionDiv, descriptionInput) => {
-    modalDescription.style.display = 'none'
-    newDescriptionDiv.classList.remove('hidden')
-    newDescriptionDiv.classList.add('new-item-description-div')
-
-    descriptionInput.value = modalDescription.textContent
-
-    // Focus on the input and select its content
-    descriptionInput.focus();
-    descriptionInput.select();
-}
-
-const updateItemDescriptionAndRefreshUI = async (modalDescription, newDescriptionDiv, descriptionInput, itemId) => {
-    try {
-        modalDescription.textContent = descriptionInput.value;
-        newDescriptionDiv.classList.remove('new-item-description-div')
-        newDescriptionDiv.classList.add('hidden')
-        modalDescription.style.display = 'block'
-
-        const response = await assets.editItemDescription(itemId, modalDescription.textContent)
-
-    } catch (error) {
-        console.error(error)
-    }
-}
-
-const handleItemDeletion = async (event, itemName, itemId, itemParentId, itemModal, parentNode, data) => {
-    const isConfirmed = confirm(`Are you sure you want to delete "${itemName}"?`);
-
-    if (isConfirmed) {
-        try {
-            const response = await assets.removeItem(itemId)
-
-            // Prevent the event from bubbling up to the room button click listener
-            event.stopPropagation();
-
-            // Remove item modal window
-            itemModal.style.display = "none";
-
-            // Rerender left menu container with updated contents
-            updateContentsInLeftMenu(itemParentId, data)
-
-            // Get item parent container contents
-            const itemParentContainerContents = data.get(parseInt(itemParentId))
-
-            // Re-render all the contents of the current container
-            updateContentsInRightContainer(parentNode, itemParentContainerContents, data)
-        } catch (error) {
-            console.error(error)
-        }
-    }
-}
-
-const handleClosingItemModalWindow = (newItemNameDiv, modalTitle, itemModal, parentNode, data, itemParentId) => {
-    newItemNameDiv.style.display = 'none'
-    modalTitle.style.display = 'block'
-
-    itemModal.style.display = "none";
-
-    const itemParentContainerContents = data.get(parseInt(itemParentId))
-
-    // Re-render all the contents of the current container
-    updateContentsInLeftMenu(itemParentId, data)
-    updateContentsInRightContainer(parentNode, itemParentContainerContents, data)
-}
-
 const handleImageUploading = () => {
     const reader = new FileReader();
     reader.onload = convertImageToBase64;
@@ -242,7 +68,7 @@ const handleImageUploading = () => {
 
 function convertImageToBase64(event) {
     const base64String = event.target.result.replace("data:", "").replace(/^.+,/, "");
-    // Save the base64String to your local Map or state here
+    return base64String
 }
 
 // Function to set up the confirmation modal
@@ -263,4 +89,4 @@ function setupConfirmationModal(entityName, confirmCallback) {
     return confirmBtn;  // Return the confirm button if needed outside this function
 }
 
-export { updateContentsInLeftMenu, updateContentsInRightContainer, renderContainerContents, buildNewContainerNameInput, replaceTitleWithEditableInput, replaceItemNameWithEditableInput, updateItemNameAndRefreshUI, replaceItemDescriptionWithEditableInput, updateItemDescriptionAndRefreshUI, handleItemDeletion, handleClosingItemModalWindow, handleImageUploading, setupConfirmationModal }
+export { updateContentsInLeftMenu, updateContentsInRightContainer, renderContainerContents, handleImageUploading, setupConfirmationModal }
