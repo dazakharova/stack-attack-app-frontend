7f6dd8be4a5ce9c61e61ecf57fa37a48f28a9b5a
Daria Zakharova <t3zada00@students.oamk.fi>
2024-05-01 21:09:48 +0300

    Update redirect function

--- js/profile/redirectToHomePage.js
+++ js/profile/redirectToHomePage.js
@@ -1,24 +1,5 @@
 import { isAuthenticated } from "./utils/auth.js";
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     // Get links to the specific sections of the homepage
-//     const links = document.querySelectorAll('.section')
-//     links.forEach(link => {
-//         // Sending auth query inside url while accessing navigations links
-//         // And dynamically display homepage header according to the user authentication status
-//         link.addEventListener.onclick = async (event) => {
-//             event.preventDefault(); // Prevent default link behavior
-//             const authStatus = await isAuthenticated();
-//             const destination = '../index.html';
-//             const sectionToScrollTo = link.getAttribute('href')// Original destination
-//             const authQuery = authStatus ? "?auth=logged" : "?auth=unlogged"; // Append authentication status
-//             window.location.href = `${destination}${authQuery}${sectionToScrollTo}`; // Navigate with authentication status
-//         };
-//     });
-// })
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const howItWorksLink = document.getElementById('mainpageLink');
     const ourTeamLink = document.getElementById('ourTeamLink');
@@ -31,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
             event.preventDefault(); // Prevent default link behavior
             const authStatus = await isAuthenticated();
             const destination = '../index.html';
-            const sectionToScrollTo = `#${link.id}`// Original destination
+            const sectionToScrollTo = `${link.getAttribute('data-href')}`// Original destination
             const authQuery = authStatus ? "?auth=logged" : "?auth=unlogged"; // Append authentication status
             window.location.href = `${destination}${authQuery}${sectionToScrollTo}`; // Navigate with authentication status
         }
