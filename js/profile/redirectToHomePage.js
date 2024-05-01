280e1e0453d0a4644660e8859d8ade4f0e3c7f63
Daria Zakharova <t3zada00@students.oamk.fi>
2024-05-01 20:54:17 +0300

    Fix redirect to homepage function

--- js/profile/redirectToHomePage.js
+++ js/profile/redirectToHomePage.js
@@ -24,17 +24,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const ourTeamLink = document.getElementById('ourTeamLink');
     const feedbackSection = document.getElementById('feedbackLink');
 
-    const links = [ howItWorksLink, ourTeamLink, feedbackSection ];
+    const links = [howItWorksLink, ourTeamLink, feedbackSection];
 
     links.forEach(link => {
         link.onclick = async (event) => {
             event.preventDefault(); // Prevent default link behavior
             const authStatus = await isAuthenticated();
             const destination = '../index.html';
-
             const sectionToScrollTo = `#${link.id}`// Original destination
-
             const authQuery = authStatus ? "?auth=logged" : "?auth=unlogged"; // Append authentication status
             window.location.href = `${destination}${authQuery}${sectionToScrollTo}`; // Navigate with authentication status
         }
     })
+})
